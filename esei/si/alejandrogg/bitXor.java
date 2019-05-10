729150a888df33d46fe8437c92f0c4e44b828186
aggarcia3 <27727839+aggarcia3@users.noreply.github.com>
2019-05-10 19:19:34 +0200

    Unificar tabulaciones y fines de línea

--- esei/si/alejandrogg/bitXor.java
+++ esei/si/alejandrogg/bitXor.java
@@ -1,47 +1,48 @@
-// Código de acción interna para el proyecto ConnectAlfa.mas2j
-
-package esei.si.alejandrogg;
-
-import jason.*;
-import jason.asSemantics.*;
-import jason.asSyntax.*;
-
-public final class bitXor extends DefaultInternalAction {
+// Código de acción interna para el proyecto ConnectAlfa.mas2j
+
+package esei.si.alejandrogg;
+
+import jason.*;
+import jason.asSemantics.*;
+import jason.asSyntax.*;
+
+public final class bitXor extends DefaultInternalAction {
     private static InternalAction instancia = null;
 
     public static InternalAction create() {
         if (instancia == null) {
             instancia = new bitXor();
-		}
+        }
         return instancia;
     }
 
     @Override
-	public int getMinArgs() {
+    public int getMinArgs() {
         return 3;
     }
 
     @Override
-	public int getMaxArgs() {
+    public int getMaxArgs() {
         return 3;
     }
 
-    @Override
-    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
-		boolean toret = true;
-		double op1;
-		double op2;
+    @Override
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+        boolean toret = true;
+        double op1;
+        double op2;
 
-		checkArguments(args);
+        checkArguments(args);
 
-		toret = args[0].isNumeric() && args[1].isNumeric();
-		if (toret) {
-			op1 = ((NumberTerm) args[0]).solve();
-			op2 = ((NumberTerm) args[1]).solve();
-			// Hacer operación XOR binaria entre dos double
-			toret = un.unifies(args[2], new NumberTermImpl(Double.longBitsToDouble(Double.doubleToRawLongBits(op1) ^ Double.doubleToRawLongBits(op2))));
-		}
-		
-		return toret;
-    }
-}
+        toret = args[0].isNumeric() && args[1].isNumeric();
+        if (toret) {
+            op1 = ((NumberTerm) args[0]).solve();
+            op2 = ((NumberTerm) args[1]).solve();
+            // Hacer operación XOR binaria entre dos double
+            toret = un.unifies(args[2], new NumberTermImpl(Double.longBitsToDouble(Double.doubleToRawLongBits(op1) ^ Double.doubleToRawLongBits(op2))));
+        }
+
+        return toret;
+    }
+}
+
--- esei/si/alejandrogg/retract.java
+++ esei/si/alejandrogg/retract.java
@@ -10,46 +10,47 @@ import jason.bb.*;
 
 public final class retract extends DefaultInternalAction {
     @Override
-	public int getMinArgs() {
+    public int getMinArgs() {
         return 1;
     }
 
     @Override
-	public int getMaxArgs() {
+    public int getMaxArgs() {
         return 1;
     }
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
-		BeliefBase bb;
-		Literal bel;
-		Iterator<Literal> it;
-		Literal actual;
-		Literal aBorrar = null;
+        BeliefBase bb;
+        Literal bel;
+        Iterator<Literal> it;
+        Literal actual;
+        Literal aBorrar = null;
 
-		checkArguments(args);
+        checkArguments(args);
 
-		bel = (Literal) args[0];
-		bb = ts.getAg().getBB();
+        bel = (Literal) args[0];
+        bb = ts.getAg().getBB();
 
-		// Basado en el código del método abolish de Agent.java
-		synchronized (bb.getLock()) {
-			it = bb.getCandidateBeliefs(bel, un);
-			if (it != null) {
-				// Encontrar y borrar el primer predicado que no sea una regla y que unifique con el primer argumento
-				while (it.hasNext() && aBorrar == null) {
-					actual = it.next();
-					if (!actual.isRule() && un.clone().unifiesNoUndo(bel, actual)) {
-						aBorrar = actual;
-					}
-				}
+        // Basado en el código del método abolish de Agent.java
+        synchronized (bb.getLock()) {
+            it = bb.getCandidateBeliefs(bel, un);
+            if (it != null) {
+                // Encontrar y borrar el primer predicado que no sea una regla y que unifique con el primer argumento
+                while (it.hasNext() && aBorrar == null) {
+                    actual = it.next();
+                    if (!actual.isRule() && un.clone().unifiesNoUndo(bel, actual)) {
+                        aBorrar = actual;
+                    }
+                }
 
-				if (aBorrar != null){
-					ts.getAg().delBel(aBorrar);
-				}
-			}
-		}
+                if (aBorrar != null){
+                    ts.getAg().delBel(aBorrar);
+                }
+            }
+        }
 
-		return true;
+        return true;
     }
 }
+
